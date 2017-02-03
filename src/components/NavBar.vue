b89c825ae659c53bdf4e32d051f0372972f02bc2
Jeremy Tribby <jeremy@tribby.com>
2017-02-03 01:13:54 -0800

    font size headings menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -182,7 +182,7 @@ nav > ul > li:not(.menu) {
 nav > ul > li > a { // headings
   line-height: 0.9em;
   color: $grey-black;
-  font-size: 1.05em;
+  font-size: 1em;
   margin-bottom: 8px;
   &:after {
     content: '';
