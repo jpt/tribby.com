913ccc898c12255a69730981a91feaac06780997
Jeremy Tribby <jeremy@tribby.com>
2017-02-08 19:40:43 -0800

    Add some teaser text

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,24 +1,26 @@
 <template lang="pug">
 div.current
   div.container
-    h1 Electronic Frontier Foundation
-    p Stuff about EFF
+    h1 Certbot
+    p While working at the Electronic Frontier Foundation as a front-end engineer and UI/UX designer, I designed and developed the the website for Certbot, EFF's cli tool for Let's Encrypt SSL certificate management.
+    h1 Turner
+    p Turner is a static site generator I built at EFF for rapid UX/UI prototyping that could easy move to production.
     h1 Barlow
-    p Stuff about Barlow
+    p Barlow is an open source, libre type family that I have designed. Coming soon to [redacted]. You can see it in use on this website in the sub-nav.
     h1 Ricochet
-    p Stuff about Ricochet
+    p I began contributing design to Ricochet in the winter. Keep your eyes peeled!
     h1 Encryptly
-    p Some stuff about Encryptly
+    p Encryptly is the second VPN company I ran for a short period of time. 
     h1 Chefs Feed
-    p Some stuff about Chefs Feed
+    p I was head of interactive at Chefs Feed, a food media company in SF.
     h1 Edupath
-    p Some stuff about Edupath
+    p I designed and built the front for the website, student, and parent portals for Edupath, an SAT prep app for iOS and Android.
     h1 ValleyVPN
-    p Some stuff about ValleyVPN
+    p ValleyVPN is a VPN company I ran for a while.
     h1 Front Drive
-    p Some stuff about Front Drive
+    p Front Drive was a web hosting company I ran in the early 2000s.
     h1 Electra Bicycle Co.
-    p Some stuff about Electra
+    p I did design and web development for Electa in the late 90s.
 
 </template>
 <style lang="sass">
