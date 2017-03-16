d414919ae8d04ff5d73da22c57a3d3d04fb4b6f5
Jeremy Tribby <jeremy@tribby.com>
2017-03-15 22:48:10 -0700

    latest font and more content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,25 +2,33 @@
 div.home
   section.blog
     article
+      h1 Almsot there.
+      h5 2017 March 15th
+      p Just need to fix the hamburger menu.
+    article
       h1 Cleanup on aisle you.
-      h5 02 Feb 2016
-      p Slowly building out a new website. I'll have some stuff from the archives, soon.
+      h5 2017 February 16th
+      p Slowly building out a new website. I'll have some stuff from the archives, soon. Mobile experience is a work in progress (hello, non-fucntioning hamburger button).
 </template>
 <style lang="sass">
+
 @import "../styles/globals.scss";
 
 section.blog {
+  article {
+    h1 {
+      margin-bottom: 0em;
+    }
 
-  h1 {
-    margin-bottom: 0em;
-  }
+    h5 {
+      line-height: 1.6em;
+      color: $grey-medium;
+    }
+    p:first-of-type {
+      margin-top: 22px;
+    }
 
-  h5 {
-    line-height: 1.6em;
-    color: $grey-medium;
-  }
-  p:first-of-type {
-    margin-top: 22px;
+    margin-bottom: 50px;
   }
 }
 </style>
