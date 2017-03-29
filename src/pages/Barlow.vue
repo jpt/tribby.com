cee4647739c3ecd16fb64aba877b55106f7aad1b
Jeremy Tribby <jeremy@tribby.com>
2017-03-29 04:26:34 -0700

    add direct download link to barlow

--- src/pages/Barlow.vue
+++ src/pages/Barlow.vue
@@ -2,5 +2,6 @@
 div.barlow
   h1 Barlow
   p #[a(href="https://github.com/jpt/barlow") Barlow] is a libre, open source typeface I am currently developing. It's named after activist, co-founding EFF internet pioneer, and songwriter John Perry Barlow.
+  p #[a(href="https://github.com/jpt/barlow/archive/master.zip") Download]
   img(src="https://raw.githubusercontent.com/jpt/barlow/master/documentation/barlow.gif")
 </template>
