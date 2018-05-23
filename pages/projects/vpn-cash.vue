1baa2104dc6c64c10c6a3f1556b72678666927e3
Jeremy Tribby <jeremy@tribby.com>
2018-05-23 05:42:29 -0700

    change vpncash language

--- pages/projects/vpn-cash.vue
+++ pages/projects/vpn-cash.vue
@@ -3,7 +3,7 @@
     <div class="vpn-cash">
       <div class="heading">
         <h1>VPN Cash</h1>
-        <p>is the first truly anonymous VPN provider, accepting only Zcash and Monero privacy coins as payment. At VPN Cash I function as head of product. There's a link to the beta <a location="_new" href="https://22fnkumzjlnjq7haha7pvnhsyolujsds.xyz/">here</a> but you probably can't have the password.</p>
+        <p>is the first truly anonymous VPN provider, accepting only Zcash and Monero privacy coins as payment. At VPN Cash I function as head of product. <a href="mailto:hello@vpn.cash">Send an email to hello@vpn.cash</a> if you'd like to join the private beta.</p>
       </div>
       <video autoplay="autoplay" loop>
 	  	<source src="~assets/comcast.mp4" type="video/mp4">
