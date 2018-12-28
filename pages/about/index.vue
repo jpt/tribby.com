eac31c11b10dbfa3fd898f86942345042058ae93
Jeremy Tribby <jeremy@tribby.com>
2018-12-28 14:07:15 -0800

    rewrite about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div class="about">
       <h1>About</h1>
-      <p>Jeremy Tribby is an artist, designer, and engineer from and based in California. He studied art history and painting at UC Berkeley. He has worked with agencies and brands like Instrument and Google, and spent a couple years as a front-end engineer at the Electronic Frontier Foundation. He recently found success as a type designer with his variable font superfamily, Barlow, which has been served over the web one billion times since its release this past winter. Most recently he has been working with VPN Cash, a mysterious crypto startup in San Francisco. You can reach Jeremy at <a href="mailto:jeremy@tribby.com">jeremy@tribby.com</a>. This website is open source software and you can find it <a href="https://github.com/jpt">on Github</a>.</p>
+      <p>Jeremy Tribby is an artist, designer, and engineer native to California. He studied art history and painting at UC Berkeley. He has worked with big companies like Google, NBC, and EFF, as well as small ones like Chefs Feed, Edupath, and Shift. Jeremy's variable font superfamily, Barlow, was published by Google and has since been served over the web billions of times.<p>
+
+      <p>Tribby.com has been on the web continuously for {{ new Date().getFullYear() - 2001 }} years, and you can find <a target="_blank" href="https://web.archive.org/web/20040417072500/http://tribby.com">all sorts of bad ideas</a> from the past on archive.org. You can reach Jeremy <a href="mailto:jeremy@tribby.com" target="_blank">via email</a> (without PGP encryption, please). This website is open source software and you can find it <a href="https://github.com/jpt" target="_blank">on Github</a>.</p>
     </div>
   </div>
 </template>
