a8b3d3285485a55aef61809909943980fb2d536d
Jeremy Tribby <jeremy@tribby.com>
2019-06-02 23:18:12 -0700

    copy edit

--- pages/fonts/index.vue
+++ pages/fonts/index.vue
@@ -6,7 +6,7 @@
 	      <p><a href="/fonts/barlow">Barlow</a> is a sans-serif neo-grotesque superfamily available in 54 styles, designed by Jeremy Tribby and published by Google Fonts. It has support for over 200 languages. Coming up next is Cyrillic support. Barlow is a free, open source font, and can be found on <a href="https://github.com/jpt/barlow" target="_new">Github</a>.</p>
 	      <img src="~assets/barlow.gif">
 	      <p><a href="/fonts/Galiano">Galiano</a> is modern-transitional serif typeface for body text currently being developed.</p>
-        <p>Tribby No. 5 is the sans you see used on this website for headings. It is under development.</p>
+        <p>Tribby Grotesk is the sans you see used on this website for headings. It is under development.</p>
   	  </div>
     </div>
   </div>
