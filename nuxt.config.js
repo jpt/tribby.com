9c9e5210e06bccc9e201e2b99f8ffea03dfd5422
Jeremy Tribby <jeremy@tribby.com>
2019-06-02 23:21:37 -0700

    metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Tribby Type: designing and and engineering typefaces, products, and digital experiences. Founded in 2001 as the creative studio of Jeremy Tribby.' }
+      { hid: 'description', name: 'description', content: 'Tribby Type: designing and and engineering typefaces, products, and digital experiences.' }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' }
