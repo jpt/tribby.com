b91a1dfa118994c1564d7f83074fb0952cc31123
Jeremy Tribby <jeremy@tribby.com>
2021-11-17 19:24:26 -0800

    about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="page about">
       <h1>About</h1>
-      <p>Jeremy Tribby is an artist, designer, and programmer from California. He is the creator of the Arthouse family of typefaces for NBC, as well as the open source Google Font, Barlow. Currently, he works as a design technologist at <a href="https://plaid.com/">Plaid</a>. In the past he worked as an engineer at the Electronic Frontier Foundation (EFF), the civil liberties non-profit, and in various roles at a number of startups. Jeremy studied art and art history at UC Berkeley.<p>
-      <p>You can reach Jeremy <a href="mailto:jeremy@tribby.com" target="_blank">via email</a> This website is open source software and you can find it <a href="https://github.com/jpt" target="_blank">on Github</a>. Tribby.com has been on the web for {{ new Date().getFullYear() - 2001 }} years, and you can find <a target="_blank" href="https://web.archive.org/web/20040417072500/http://tribby.com">all sorts of bad ideas</a> from the past on the Internet Archive's Wayback Machine.</p>
+      <p>Jeremy Tribby is an artist, designer, and programmer from California. He is the creator of the Arthouse family of typefaces for NBCUniversal, which serves local NBC and Telemundo stations around the world. He also created Barlow, a straight-sided titling font family for Google Fonts, where it has been served 60Currently, he works as a design technologist at <a href="https://plaid.com/">Plaid</a>. In the past he worked as an engineer at the Electronic Frontier Foundation (EFF), the civil liberties non-profit, and in various design and engineering roles at a number of startups. He also runs <a href="https://countertype.com">Countertype</a> and has a very large sans-serif type family on the way. Jeremy studied art and art history at UC Berkeley and later completed Type West.<p>
+      <p>You can reach Jeremy <a href="mailto:jeremy@tribby.com" target="_blank">via email</a>.
     </div>
   </div>
 </template>
